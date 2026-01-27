bd9deb3c44b35e693bd01c9f136eee6dea320546
Alban Dericbourg <alban@dericbourg.dev>
2026-01-27 20:18:52 +0100

    Optimize images
    And add a favicon
    
    Signed-off-by: Alban Dericbourg <alban@dericbourg.dev>

--- src/components/ui/Layout/Header.tsx
+++ src/components/ui/Layout/Header.tsx
@@ -3,7 +3,7 @@ import './Header.css';
 export function Header() {
   return (
     <header className="header">
-      <img src={`${import.meta.env.BASE_URL}logo.png`} alt="" className="header__logo" />
+      <img src={`${import.meta.env.BASE_URL}logo.webp`} alt="" className="header__logo" />
       <span className="header__title">AD COOP | ALBAN DERICBOURG</span>
     </header>
   );
